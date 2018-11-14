874a9ecf98c24de6b496e0ddbf8df052a6d44860
phoebe7979 <phoebe7979@gmail.com>
2018-11-13 22:12:20 -0500

    added more content

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-/*Top navigation toggle menu*/
-function navIconClick() {
-    var x = document.getElementById("nav-tabs");
-    if (x.className === "topnav") {
-        x.className += " topnav-expand";
-    } else {
-        x.className = "topnav";
-    }
-}
